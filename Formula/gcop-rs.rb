3b6899f2fd7c3a3e587215c74c25a3b0cf63c65f
AptS:1547 <esaps@esaps.net>
2026-03-22 16:48:28 +0800

    chore: bump gcop-rs to v0.13.9

--- Formula/gcop-rs.rb
+++ Formula/gcop-rs.rb
@@ -4,13 +4,13 @@
 class GcopRs < Formula
   desc "AI-powered Git commit message generator and code reviewer"
   homepage "https://github.com/AptS-1547/gcop-rs"
-  version "0.13.8"
+  version "0.13.9"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/AptS-1547/gcop-rs/releases/download/v#{version}/gcop-rs-v#{version}-macos-arm64"
-      sha256 "f1779772e01fedf5dfb8c63089287651143f75ec0224bbb5ac4f9119133a5ac8"
+      sha256 "842315f932e038fd50afba3311738a1b77dbaeae0ec8b02ebcb4ec6b8388ceba"
 
       def install
         bin.install "gcop-rs-v#{version}-macos-arm64" => "gcop-rs"
@@ -19,7 +19,7 @@ class GcopRs < Formula
 
     on_intel do
       url "https://github.com/AptS-1547/gcop-rs/releases/download/v#{version}/gcop-rs-v#{version}-macos-amd64"
-      sha256 "b52c390d090b1025147acd112306e404f7ad43c91b764f94368291728e80cd6d"
+      sha256 "418cd94c271f29a89a9af823b7dded92109eb18afd9651c67ae6dd1b58f90ee5"
 
       def install
         bin.install "gcop-rs-v#{version}-macos-amd64" => "gcop-rs"
@@ -30,7 +30,7 @@ class GcopRs < Formula
   on_linux do
     on_arm do
       url "https://github.com/AptS-1547/gcop-rs/releases/download/v#{version}/gcop-rs-v#{version}-linux-arm64"
-      sha256 "e7a321c2d2b726837633bd4fe7f9b94b6e3d87cd25c76d84e5c9514949f02126"
+      sha256 "07043da42f042aca118bf7e191a88a7932173c5694ce478c8e56fbc3c5e89189"
 
       def install
         bin.install "gcop-rs-v#{version}-linux-arm64" => "gcop-rs"
@@ -39,7 +39,7 @@ class GcopRs < Formula
 
     on_intel do
       url "https://github.com/AptS-1547/gcop-rs/releases/download/v#{version}/gcop-rs-v#{version}-linux-amd64"
-      sha256 "346ff26736a3d5408068f2a4d51f6e74597a89d7af4b7d56b796e51f07574525"
+      sha256 "b92985b7d5d64cb010d6cf39af21178a67c49a4c36913581fa0f77afcc82b29c"
 
       def install
         bin.install "gcop-rs-v#{version}-linux-amd64" => "gcop-rs"
